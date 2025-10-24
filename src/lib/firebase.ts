c088ef36009c1836145253f1bcbdcd761bc1e684
karivaradhan <164014617+Karivaradhan7@users.noreply.github.com>
2025-10-24 18:48:46 +0530

    Updated firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
 
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBeBRq1z-diZGRuxSonsI4Jn-9UQ8v8IU0",
-  authDomain: "aptora-4108d.firebaseapp.com",
-  projectId: "aptora-4108d",
-  storageBucket: "aptora-4108d.firebasestorage.app",
-  messagingSenderId: "966897977550",
-  appId: "1:966897977550:web:8f8910927011ab9225689f",
-  measurementId: "G-SWFKT9VK3P"
+  apiKey: "AIzaSyCxzIIs-VQ8Q3Z5rp1Mcx4Qk-HRveUlUdU",
+  authDomain: "aptora-58a5d.firebaseapp.com",
+  projectId: "aptora-58a5d",
+  storageBucket: "aptora-58a5d.firebasestorage.app",
+  messagingSenderId: "906991340467",
+  appId: "1:906991340467:web:39bb82415d5e5b99ced9c6",
+  measurementId: "G-BVP8FE0D4J"
 };
 
-export { firebaseConfig };
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const storage = getStorage(app);
+
+export { firebaseConfig, app, analytics, storage };
